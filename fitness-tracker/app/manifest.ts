43b08f8292e402ca347ab962b6a72cca6245be7c
Lokesh Gowda V <lokivenkatesh05@gmail.com>
2026-09-10 16:31:08 +0530

    Fix PWA manifest build type

--- fitness-tracker/app/manifest.ts
+++ fitness-tracker/app/manifest.ts
@@ -9,6 +9,6 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#f5f7fa",
     theme_color: "#111827",
-    icons: [{ src: "/icon.svg", sizes: "any", type: "image/svg+xml", purpose: "any maskable" }],
+    icons: [{ src: "/icon.svg", sizes: "any", type: "image/svg+xml", purpose: "maskable" }],
   }
 }
