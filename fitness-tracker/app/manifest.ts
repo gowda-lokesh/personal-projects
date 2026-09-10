6be4b5f5971b80f0639954d0fe5d5e908dbff0b8
Lokesh Gowda V <lokivenkatesh05@gmail.com>
2026-09-10 16:25:43 +0530

    Configure PWA app icon

--- fitness-tracker/app/manifest.ts
+++ fitness-tracker/app/manifest.ts
@@ -9,6 +9,6 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#f5f7fa",
     theme_color: "#111827",
-    icons: [],
+    icons: [{ src: "/icon.svg", sizes: "any", type: "image/svg+xml", purpose: "any maskable" }],
   }
 }
